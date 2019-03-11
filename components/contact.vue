2eab7567dbed4aeb59c2a2108435c9ac51f678de
Max <max.huber98@hotmail.com>
2019-03-11 19:45:20 +0100

    Skills, contact & header more responsive

--- components/contact.vue
+++ components/contact.vue
@@ -1,24 +1,26 @@
 <template>
   <section id="contact" class="contact">
-    <h1 class="contact__title">Contact</h1>
+    <div class="contact__wrapper">
+      <h1 class="contact__title">Contact</h1>
 
-    <div class="contact__form">
-      <form type="post" id="contactForm">
-        <div class="contact__row">
-          <input name="firstName" type="text" class="contact__textfield" placeholder="Voornaam" required>
+      <div class="contact__form">
+        <form type="post" id="contactForm">
+          <div class="contact__row">
+            <input name="firstName" type="text" class="contact__textfield" placeholder="Voornaam" required>
 
-          <input name="lastName" type="text" class="contact__textfield" placeholder="Achternaam" required>
-        </div>
-        <div class="contact__row">
-          <input name="email" type="email" class="contact__textfield contact__textfield--email" placeholder="Email" required>
-        </div>
-        <div class="contact__row">
-          <textarea type="text" name="message" class="contact__textfield contact__textfield--textarea" placeholder="Bericht" required></textarea>
-        </div>
-        <div class="contact__row">
-          <button type="submit" class="contact__submitbtn">Verzenden</button>
-        </div>
-      </form>
+            <input name="lastName" type="text" class="contact__textfield" placeholder="Achternaam" required>
+          </div>
+          <div class="contact__row">
+            <input name="email" type="email" class="contact__textfield contact__textfield--email" placeholder="Email" required>
+          </div>
+          <div class="contact__row">
+            <textarea type="text" name="message" class="contact__textfield contact__textfield--textarea" placeholder="Bericht" required></textarea>
+          </div>
+          <div class="contact__row">
+            <button type="submit" class="contact__submitbtn">Verzenden</button>
+          </div>
+        </form>
+      </div>
     </div>
   </section>
 </template>
--- components/skills.vue
+++ components/skills.vue
@@ -1,53 +1,56 @@
 <template>
   <section id="skills" class="skills">
-    <h1 class="skills__title">Skills</h1>
+    <div class="skills__wrapper">
+      <h1 class="skills__title">Skills</h1>
 
-    <div class="skills__container">
-      <div class="skills__row">
-        <h1 class="skills__name">HTML</h1>
-        <div class="skills__bar-placeholder">
-          <div class="skills__bar" style="width:85%;">
-            <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+      <div class="skills__container">
+        <div class="skills__row">
+          <h1 class="skills__name">HTML</h1>
+          <div class="skills__bar-placeholder">
+            <div class="skills__bar" style="width:85%;">
+              <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="skills__row">
-        <h1 class="skills__name">C#</h1>
-        <div class="skills__bar-placeholder">
-          <div class="skills__bar" style="width:80%;">
-            <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+        <div class="skills__row">
+          <h1 class="skills__name">C#</h1>
+          <div class="skills__bar-placeholder">
+            <div class="skills__bar" style="width:80%;">
+              <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="skills__row">
-        <h1 class="skills__name">JavaScript</h1>
-        <div class="skills__bar-placeholder">
-          <div class="skills__bar" style="width:75%;">
-            <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+        <div class="skills__row">
+          <h1 class="skills__name">JavaScript</h1>
+          <div class="skills__bar-placeholder">
+            <div class="skills__bar" style="width:75%;">
+              <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="skills__row">
-        <h1 class="skills__name">CSS</h1>
-        <div class="skills__bar-placeholder">
-          <div class="skills__bar" style="width:70%;">
-            <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+        <div class="skills__row">
+          <h1 class="skills__name">CSS</h1>
+          <div class="skills__bar-placeholder">
+            <div class="skills__bar" style="width:70%;">
+              <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="skills__row">
-        <h1 class="skills__name">PHP</h1>
-        <div class="skills__bar-placeholder">
-          <div class="skills__bar" style="width:65%;">
-            <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+        <div class="skills__row">
+          <h1 class="skills__name">PHP</h1>
+          <div class="skills__bar-placeholder">
+            <div class="skills__bar" style="width:65%;">
+              <span class="skills__bar-inner" :class="{ 'skills__bar-inner--visible': visible}"></span>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    
   </section>
 </template>
 
